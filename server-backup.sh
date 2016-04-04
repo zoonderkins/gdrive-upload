369c773d68b00c40321eae3f91579bd7cedb0011
Edward Oo <ookangzheng@users.noreply.github.com>
2016-04-05 01:23:40 +0800

    Update server-backup.sh

--- server-backup.sh
+++ server-backup.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # tar the server directory
-tar -cvf "server3-backup-$(date '+%Y-%m-%d').tar" /home/wwwroot
+tar -cvf "server-backup-$(date '+%Y-%m-%d').tar" /home/wwwroot
 
 # encrypt the tar
-#openssl aes-256-cbc -a -salt -in "server3-backup-$(date '+%Y-%m-%d').tar" -out "server3-backup-$(date '+%Y-%m-%d').tar.enc" -pass 'pass:1qsNodXNaWq1mQuBjUjmvhoO'
-
+#if you need encrpyt ur file, enable this function
+#openssl aes-256-cbc -a -salt -in "server-backup-$(date '+%Y-%m-%d').tar" -out "server-backup-$(date '+%Y-%m-%d').tar.enc" -pass 'pass:xxxxx'
+# -pass: need change to ur google drive password
 # upload to google drive
 drive upload --file "server3-backup-$(date '+%Y-%m-%d').tar"
 #drive upload --file "server3-backup-$(date '+%Y-%m-%d').tar.enc"
 
 # remove the original tar
-rm -rf "server3-backup-$(date '+%Y-%m-%d').tar"
+rm -rf "server-backup-$(date '+%Y-%m-%d').tar"
 
 
